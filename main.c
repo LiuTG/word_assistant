71bfb2670df46fecd1e164323b6c48e01717930b
LiuTG <603085386@qq.com>
2015-05-30 11:59:19 +0800

    add some Notes

--- main.c
+++ main.c
@@ -1,30 +1,36 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 int main(void)
 {
 	 //指示器
 	 int i;
 	 FILE * pFile;
+	 //每次读取一行的临时存储
 	 char content [100];
 
 	 pFile = fopen ("1.txt" , "r");
+
 	 if (pFile == NULL) 
 		 perror("Error opening file");
      else 
 	 {
-		
+		//循环读取文章，直至结束
 		while(fgets(content , 100 , pFile)!= NULL )
 		{
 			for(i=0;i<100;i++)
 			{
 				//if(isspace(content[i]))
+				//使用Ascii码从中跳出大小写的字母
 				if(((content[i]>='a')&&(content[i]<='z'))||((content[i]>='A')&&(content[i]<='Z')))			
 				{
+					char temp [30];
 					printf("%c",content[i]);
 					
 				}
-				else if(content[i]==' ')
+				else
 				{
+
 					printf("\n");
 				}
 
